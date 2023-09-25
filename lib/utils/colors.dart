c6aca4fad1a4ef25d3f7e60f135970557e020466
Kelvin Firmino <firminojnr@icloud.com>
2023-09-25 11:41:09 -0300

    first commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// Dark Colors
+class DarkColors {
+  static const scaffoldColor = Color(0xff22252D);
+  static const sheetBgColor = Color(0xff292D36);
+  static const btnColor = Color.fromARGB(255, 33, 36, 42);
+  static const leftOperatorColor = Color.fromARGB(255, 7, 255, 209);
+}
+
+/// Light Colors
+class LightColors {
+  static const scaffoldColor = Color(0xffFFFFFF);
+  static const sheetBgColor = Color(0xffF9F9F9);
+  static const btnBgColor = Color.fromARGB(255, 243, 243, 243);
+  static const leftOperatorColor = Color.fromARGB(255, 1, 157, 127);
+}
